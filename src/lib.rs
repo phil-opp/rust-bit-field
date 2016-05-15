f51c6c0e287986889e2d679ad2cf30dcc901409d
Philipp Oppermann <dev@phil-opp.com>
2016-05-15 14:23:40 +0200

    Make BitField generic over u8-u64

--- src/lib.rs
+++ src/lib.rs
@@ -1,113 +1,79 @@
+#![feature(zero_one)]
 #![no_std]
 
-use core::ops::Range;
+#[cfg(test)]
+mod tests;
+
+use core::mem::size_of;
 
 #[derive(Debug, Clone, Copy)]
-pub struct BitField(u32);
+pub struct BitField<T: Number>(T);
 
-impl BitField {
-    pub fn new(value: u32) -> BitField {
+impl<T> BitField<T>
+    where T: Number
+{
+    pub fn new(value: T) -> BitField<T> {
         BitField(value)
     }
 
-    pub fn get_bit(&self, bit: u32) -> bool {
-        assert!(bit < 32);
-        self.get_range(bit..(bit+1)) == 1
+    pub fn get_bit(&self, bit: usize) -> bool {
+        assert!(bit < self.length());
+        self.get_range(bit..(bit + 1)) == T::one()
     }
 
-    pub fn get_range(&self, range: Range<u32>) -> u32 {
-        assert!(range.start < 32);
-        assert!(range.end <= 32);
+    pub fn get_range(&self, range: Range<usize>) -> T {
+        assert!(range.start < self.length());
+        assert!(range.end <= self.length());
         assert!(range.start < range.end);
 
         // shift away high bits
-        let bits = self.0 << (32 - range.end) >> (32 - range.end);
+        let bits = self.0 << (self.length() - range.end) >> (self.length() - range.end);
+
         // shift away low bits
         bits >> range.start
     }
 
-    pub fn set_bit(&mut self, bit: u32) {
-        assert!(bit < 32);
-        self.0 |= 1 << bit;
+    pub fn set_bit(&mut self, bit: usize) {
+        assert!(bit < self.length());
+        self.0 |= T::one() << bit;
     }
 
-    pub fn reset_bit(&mut self, bit: u32) {
-        assert!(bit < 32);
-        self.0 &= !(1 << bit);
+    pub fn reset_bit(&mut self, bit: usize) {
+        assert!(bit < self.length());
+        self.0 &= !(T::one() << bit);
     }
 
-    pub fn set_range(&mut self, range: Range<u32>, value: u32) {
-        assert!(range.start < 32);
-        assert!(range.end <= 32);
+    pub fn set_range(&mut self, range: Range<usize>, value: T) {
+        assert!(range.start < self.length());
+        assert!(range.end <= self.length());
         assert!(range.start < range.end);
-        assert!(value << (32 - (range.end - range.start)) >> (32 - (range.end - range.start)) == value, "value too big");
+        assert!(value << (self.length() - (range.end - range.start)) >> (self.length() - (range.end - range.start)) ==
+                value,
+                "value too big");
 
-        let bitmask = !(!0 << (32 - range.end) >> (32 - range.end) >> range.start << range.start);
+        let bitmask: T = !(!T::zero() << (self.length() - range.end) >> (self.length() - range.end) >> range.start <<
+                           range.start);
 
         let bits = self.0 & bitmask;
         // set bits
         self.0 = bits | (value << range.start);
     }
-}
 
-#[cfg(test)]
-mod tests {
-    use BitField;
-
-    #[test]
-    fn test_read() {
-        let field = BitField(0b1111111111010110);
-        assert_eq!(field.get_bit(0), false);
-        assert_eq!(field.get_bit(1), true);
-        assert_eq!(field.get_bit(2), true);
-        assert_eq!(field.get_bit(3), false);
-        assert_eq!(field.get_bit(4), true);
-        assert_eq!(field.get_bit(5), false);
-        for i in 6..16 {
-            assert_eq!(field.get_bit(i), true);
-        }
-        for i in 16..32 {
-            assert_eq!(field.get_bit(i), false);
-        }
-
-        assert_eq!(field.get_range(16..32), 0);
-        assert_eq!(field.get_range(6..16), 0b1111111111);
-        assert_eq!(field.get_range(0..6), 0b010110);
-        assert_eq!(field.get_range(0..10), 0b1111010110);
-        assert_eq!(field.get_range(5..12), 0b1111110);
-    }
-
-    #[test]
-    fn test_set_reset() {
-        let mut field = BitField(0b1111111111010110);
-        let mut bit_i = |i| {
-            field.set_bit(i);
-            assert_eq!(field.get_bit(i), true);
-            field.reset_bit(i);
-            assert_eq!(field.get_bit(i), false);
-            field.set_bit(i);
-            assert_eq!(field.get_bit(i), true);
-        };
-        for i in 0..32 {
-            bit_i(i);
-        }
-    }
-
-    #[test]
-    fn test_set_range() {
-        let mut field = BitField(0b1111111111010110);
-        field.set_range(10..15, 0b00000);
-        assert_eq!(field.get_range(10..15), 0b00000);
-        field.set_range(10..15, 0b10101);
-        assert_eq!(field.get_range(10..15), 0b10101);
-        field.set_range(10..15, 0b01010);
-        assert_eq!(field.get_range(10..15), 0b01010);
-        field.set_range(10..15, 0b11111);
-        assert_eq!(field.get_range(10..15), 0b11111);
-
-        field.set_range(0..16, 0xdead);
-        field.set_range(14..32, 0xbeaf);
-        assert_eq!(field.get_range(0..16), 0xdead);
-        assert_eq!(field.get_range(14..32), 0xbeaf);
+    fn length(&self) -> usize {
+        size_of::<T>() * 8
     }
 }
+
+use core::ops::{Range, Shl, Shr, BitAnd, BitOr, BitOrAssign, BitAndAssign, Not};
+use core::num::{Zero, One};
+use core::fmt::Debug;
+
+pub trait Number: Debug + Copy + Eq + Zero + One +
+    Not<Output=Self> + Shl<usize, Output=Self> + Shr<usize, Output=Self> +
+    BitAnd<Self, Output=Self> + BitOr<Self, Output=Self>  + BitAndAssign + BitOrAssign {}
+
+impl Number for u8 {}
+impl Number for u16 {}
+impl Number for u32 {}
+impl Number for u64 {}
+impl Number for usize {}
---
+++ src/tests.rs
@@ -0,0 +1,153 @@
+use BitField;
+
+#[test]
+fn test_set_reset_u8() {
+    let mut field = BitField(0b11110010u8);
+    let mut bit_i = |i| {
+        field.set_bit(i);
+        assert_eq!(field.get_bit(i), true);
+        field.reset_bit(i);
+        assert_eq!(field.get_bit(i), false);
+        field.set_bit(i);
+        assert_eq!(field.get_bit(i), true);
+    };
+    for i in 0..8 {
+        bit_i(i);
+    }
+}
+
+#[test]
+fn test_set_reset_u16() {
+    let mut field = BitField(0b1111001010010110u16);
+    let mut bit_i = |i| {
+        field.set_bit(i);
+        assert_eq!(field.get_bit(i), true);
+        field.reset_bit(i);
+        assert_eq!(field.get_bit(i), false);
+        field.set_bit(i);
+        assert_eq!(field.get_bit(i), true);
+    };
+    for i in 0..16 {
+        bit_i(i);
+    }
+}
+
+#[test]
+fn test_read_u32() {
+    let field = BitField(0b1111111111010110u32);
+    assert_eq!(field.get_bit(0), false);
+    assert_eq!(field.get_bit(1), true);
+    assert_eq!(field.get_bit(2), true);
+    assert_eq!(field.get_bit(3), false);
+    assert_eq!(field.get_bit(4), true);
+    assert_eq!(field.get_bit(5), false);
+    for i in 6..16 {
+        assert_eq!(field.get_bit(i), true);
+    }
+    for i in 16..32 {
+        assert_eq!(field.get_bit(i), false);
+    }
+
+    assert_eq!(field.get_range(16..32), 0);
+    assert_eq!(field.get_range(6..16), 0b1111111111);
+    assert_eq!(field.get_range(0..6), 0b010110);
+    assert_eq!(field.get_range(0..10), 0b1111010110);
+    assert_eq!(field.get_range(5..12), 0b1111110);
+}
+
+#[test]
+fn test_set_reset_u32() {
+    let mut field = BitField(0b1111111111010110u32);
+    let mut bit_i = |i| {
+        field.set_bit(i);
+        assert_eq!(field.get_bit(i), true);
+        field.reset_bit(i);
+        assert_eq!(field.get_bit(i), false);
+        field.set_bit(i);
+        assert_eq!(field.get_bit(i), true);
+    };
+    for i in 0..32 {
+        bit_i(i);
+    }
+}
+
+#[test]
+fn test_set_range_u32() {
+    let mut field = BitField(0b1111111111010110u32);
+    field.set_range(10..15, 0b00000);
+    assert_eq!(field.get_range(10..15), 0b00000);
+    field.set_range(10..15, 0b10101);
+    assert_eq!(field.get_range(10..15), 0b10101);
+    field.set_range(10..15, 0b01010);
+    assert_eq!(field.get_range(10..15), 0b01010);
+    field.set_range(10..15, 0b11111);
+    assert_eq!(field.get_range(10..15), 0b11111);
+
+    field.set_range(0..16, 0xdead);
+    field.set_range(14..32, 0xbeaf);
+    assert_eq!(field.get_range(0..16), 0xdead);
+    assert_eq!(field.get_range(14..32), 0xbeaf);
+}
+
+#[test]
+fn test_read_u64() {
+    let field = BitField(0b1111111111010110u64 << 32);
+    for i in 0..32 {
+        assert_eq!(field.get_bit(i), false);
+    }
+    assert_eq!(field.get_bit(32), false);
+    assert_eq!(field.get_bit(33), true);
+    assert_eq!(field.get_bit(34), true);
+    assert_eq!(field.get_bit(35), false);
+    assert_eq!(field.get_bit(36), true);
+    assert_eq!(field.get_bit(37), false);
+    for i in 38..48 {
+        assert_eq!(field.get_bit(i), true);
+    }
+    for i in 48..64 {
+        assert_eq!(field.get_bit(i), false);
+    }
+
+    assert_eq!(field.get_range(0..32), 0);
+    assert_eq!(field.get_range(48..64), 0);
+    assert_eq!(field.get_range(38..48), 0b1111111111);
+    assert_eq!(field.get_range(32..38), 0b010110);
+    assert_eq!(field.get_range(32..42), 0b1111010110);
+    assert_eq!(field.get_range(37..44), 0b1111110);
+}
+
+#[test]
+fn test_set_reset_u64() {
+    let mut field = BitField(0b1111111111010110u64 << 32);
+    let mut bit_i = |i| {
+        field.set_bit(i);
+        assert_eq!(field.get_bit(i), true);
+        field.reset_bit(i);
+        assert_eq!(field.get_bit(i), false);
+        field.set_bit(i);
+        assert_eq!(field.get_bit(i), true);
+    };
+    for i in 0..64 {
+        bit_i(i);
+    }
+}
+
+#[test]
+fn test_set_range_u64() {
+    let mut field = BitField(0b1111111111010110u64 << 32);
+    field.set_range(42..47, 0b00000);
+    assert_eq!(field.get_range(42..47), 0b00000);
+    field.set_range(10..15, 0b10101);
+    assert_eq!(field.get_range(10..15), 0b10101);
+    field.set_range(40..45, 0b01010);
+    assert_eq!(field.get_range(40..45), 0b01010);
+    field.set_range(40..45, 0b11111);
+    assert_eq!(field.get_range(40..45), 0b11111);
+
+    field.set_range(0..16, 0xdead);
+    field.set_range(14..32, 0xbeaf);
+    field.set_range(32..64, 0xcafebabe);
+    assert_eq!(field.get_range(0..16), 0xdead);
+    assert_eq!(field.get_range(14..32), 0xbeaf);
+    assert_eq!(field.get_range(32..64), 0xcafebabe);
+}
