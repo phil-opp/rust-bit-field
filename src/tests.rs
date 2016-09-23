96794cc0c14182c06e49410245520a82a819dc7f
Philipp Oppermann <dev@phil-opp.com>
2016-09-23 16:16:55 +0200

    Fix tests: The BitField wrapper type no longer exists

--- src/tests.rs
+++ src/tests.rs
@@ -2,7 +2,7 @@ use BitField;
 
 #[test]
 fn test_set_reset_u8() {
-    let mut field = BitField(0b11110010u8);
+    let mut field = 0b11110010u8;
     let mut bit_i = |i| {
         field.set_bit(i, true);
         assert_eq!(field.get_bit(i), true);
@@ -18,7 +18,7 @@ fn test_set_reset_u8() {
 
 #[test]
 fn test_set_reset_u16() {
-    let mut field = BitField(0b1111001010010110u16);
+    let mut field = 0b1111001010010110u16;
     let mut bit_i = |i| {
         field.set_bit(i, true);
         assert_eq!(field.get_bit(i), true);
@@ -34,7 +34,7 @@ fn test_set_reset_u16() {
 
 #[test]
 fn test_read_u32() {
-    let field = BitField(0b1111111111010110u32);
+    let field = 0b1111111111010110u32;
     assert_eq!(field.get_bit(0), false);
     assert_eq!(field.get_bit(1), true);
     assert_eq!(field.get_bit(2), true);
@@ -57,7 +57,7 @@ fn test_read_u32() {
 
 #[test]
 fn test_set_reset_u32() {
-    let mut field = BitField(0b1111111111010110u32);
+    let mut field = 0b1111111111010110u32;
     let mut bit_i = |i| {
         field.set_bit(i, true);
         assert_eq!(field.get_bit(i), true);
@@ -73,7 +73,7 @@ fn test_set_reset_u32() {
 
 #[test]
 fn test_set_range_u32() {
-    let mut field = BitField(0b1111111111010110u32);
+    let mut field = 0b1111111111010110u32;
     field.set_range(10..15, 0b00000);
     assert_eq!(field.get_range(10..15), 0b00000);
     field.set_range(10..15, 0b10101);
@@ -91,7 +91,7 @@ fn test_set_range_u32() {
 
 #[test]
 fn test_read_u64() {
-    let field = BitField(0b1111111111010110u64 << 32);
+    let field = 0b1111111111010110u64 << 32;
     for i in 0..32 {
         assert_eq!(field.get_bit(i), false);
     }
@@ -118,7 +118,7 @@ fn test_read_u64() {
 
 #[test]
 fn test_set_reset_u64() {
-    let mut field = BitField(0b1111111111010110u64 << 32);
+    let mut field = 0b1111111111010110u64 << 32;
     let mut bit_i = |i| {
         field.set_bit(i, true);
         assert_eq!(field.get_bit(i), true);
@@ -134,7 +134,7 @@ fn test_set_reset_u64() {
 
 #[test]
 fn test_set_range_u64() {
-    let mut field = BitField(0b1111111111010110u64 << 32);
+    let mut field = 0b1111111111010110u64 << 32;
     field.set_range(42..47, 0b00000);
     assert_eq!(field.get_range(42..47), 0b00000);
     field.set_range(10..15, 0b10101);
