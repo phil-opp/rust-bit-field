5021f46b5627e38635f932d3b8c00c3a96cdb013
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-06-28 16:12:21 +0200

    Added some tests to get things going

--- src/tests.rs
+++ src/tests.rs
@@ -1,4 +1,5 @@
 use BitField;
+use BitArray;
 
 #[test]
 fn test_integer_bit_lengths() {
@@ -163,4 +164,35 @@ fn test_set_range_u64() {
     assert_eq!(field.get_bits(0..16), 0xdead);
     assert_eq!(field.get_bits(14..32), 0xbeaf);
     assert_eq!(field.get_bits(32..64), 0xcafebabe);
+}
+
+#[test]
+fn test_array_length() {
+    assert_eq!((&[2u8, 3u8, 4u8]).bit_length(), 24);
+    assert_eq!((&[2i8, 3i8, 4i8, 5i8]).bit_length(), 32);
+
+    assert_eq!((&[2u16, 3u16, 4u16]).bit_length(), 48);
+    assert_eq!((&[2i16, 3i16, 4i16, 5i16]).bit_length(), 64);
+    
+    assert_eq!((&[2u32, 3u32, 4u32]).bit_length(), 96);
+    assert_eq!((&[2i32, 3i32, 4i32, 5i32]).bit_length(), 128);
+
+    assert_eq!((&[2u64, 3u64, 4u64]).bit_length(), 192);
+    assert_eq!((&[2i64, 3i64, 4i64, 5i64]).bit_length(), 256);
+}
+
+#[test]
+fn test_get_array() {
+    let mut test_val = [0xffu8];
+    &test_val.set_bit(0,false);
+    assert_eq!(test_val, [0xfeu8]);
+    &test_val.set_bit(4,false);
+    assert_eq!(test_val, [0xeeu8]);
+
+    let mut test_array = [0xffu8, 0x00u8, 0xffu8];
+    &test_array.set_bit(7, false);
+    &test_array.set_bit(8, true);
+    &test_array.set_bit(16, false);
+
+    assert_eq!(test_array, [0x7fu8, 0x01u8, 0xfeu8]);
 }
